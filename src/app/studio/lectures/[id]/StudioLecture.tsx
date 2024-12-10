0644bed1067c8e8b30b2507b4a282b73c2f7d17c
Dreta <32196824+Dreta@users.noreply.github.com>
2024-12-10 10:08:29 +0800

    feat: add message to avoid confusion

--- src/app/studio/lectures/[id]/StudioLecture.tsx
+++ src/app/studio/lectures/[id]/StudioLecture.tsx
@@ -1,21 +1,45 @@
 'use client'
 
 import { useTranslationClient } from '@/app/i18n/client'
-import { TabItem, Tabs, TabsRef } from 'flowbite-react'
+import {Alert, TabItem, Tabs, TabsRef} from 'flowbite-react'
 import { HiCalendar, HiChartPie, HiDocumentText, HiRefresh, HiUsers } from 'react-icons/hi'
 import { HydratedLecture } from '@/app/lib/lecture-actions'
-import { useRef } from 'react'
+import {useEffect, useRef, useState} from 'react'
 import LectureDashboard from '@/app/studio/lectures/[id]/LectureDashboard'
 import LectureTasksC from '@/app/studio/lectures/[id]/LectureTasks'
 import LectureUsers from '@/app/studio/lectures/[id]/LectureUsers'
+import If from '@/app/lib/If'
+import {User} from '@prisma/client'
+import {getMyUser} from '@/app/login/login-actions'
 
 export default function StudioLecture({ lecture }: { lecture: HydratedLecture }) {
     const { t } = useTranslationClient('studio')
     const tabsRef = useRef<TabsRef>(null)
 
+    const [ myUser, setMyUser ] = useState<User>()
+
+    useEffect(() => {
+        (async () => {
+            setMyUser((await getMyUser())!)
+        })()
+    }, [])
+
+    if (myUser == null) {
+        return <div className="base-studio-page">
+            <div className="w-full">
+                <div className="w-1/3 h-8 bg-gray-300 dark:bg-gray-700 rounded-3xl mb-3"/>
+                <div className="w-1/2 h-8 bg-gray-300 dark:bg-gray-700 rounded-3xl mb-3"/>
+                <div className="w-full h-8 bg-gray-300 dark:bg-gray-700 rounded-3xl mb-3"/>
+            </div>
+        </div>
+    }
+
     return <div className="base-studio-page">
         <h1 className="mb-3">{lecture.title}</h1>
         <p className="text-xl mb-5 secondary">{lecture.user.name}</p>
+        <If condition={myUser.id !== lecture.user.id}>
+            <Alert color="info">{t('lecture.userAlert')}</Alert>
+        </If>
         <Tabs aria-label={t('lecture.tabs.title')} variant="underline" ref={tabsRef}>
             <TabItem active title={t('lecture.tabs.dashboard')} icon={HiChartPie}>
                 <LectureDashboard lecture={lecture} tabsRef={tabsRef.current!}/>
